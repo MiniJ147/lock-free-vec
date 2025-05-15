d5cf977a55450a15fb4a2330c874609ccfd895b8
minij147 <jakepaul628@gmail.com>
2025-05-15 15:38:50 -0400

    random access operations done with vector init at 1 bucket with default bucket size

--- lf_vec.h
+++ lf_vec.h
@@ -1,19 +1,54 @@
 #ifndef LF_VEC_H
 #define LF_VEC_H
+#include <cassert>
+#include <climits>
 #include <iostream>
 
-namespace lf {
+#define FIRST_BUCKET_SIZE 8 // as stated in 3.3 operations
+
+// NOTE:
+// we can replace the HighestBit instruciton with std::bit_width(x) in C++ 20
+int highest_bit(int x){
+    int res = std::__bit_width(x);
+
+    // result is zero so we don't want to send negative index
+    assert(res!=0);
+
+    // return the actual index of the bit (0 based) as BSR documentation states
+    // since __bit_width returns the position in (1 based) indexing
+    return res - 1;
+}
+
+namespace lockfree {
 // contains the logic and functions necessary to complete vector operations
 // what the user will use at an abstract level
 template <typename T>
 class Vector {
 private:
     // pointer array to valid blocks of memory 
-    T** data; 
+    T** memory; 
 
-    T* at(size_t idx);
+    // indexes into our array at the specfic spot we need with clever bitwise operations
+    // simply put, our bucket size grows in powers of 8 (assuming 8 is the first bucket size)
+    // we can then use the MSB to mark the number of buckets we currently have in the array 
+    // with that we can mask to find the specfic element in that array section for that bucket
+    T* at(int idx){
+        int pos = idx + FIRST_BUCKET_SIZE; // get our requested position
+        int hibit = highest_bit(pos); 
+
+        // translate this pos into an index for our array section in the bucket 
+        // (trimming the MSB)
+        int new_idx = pos ^ (1<<hibit); // 1<<(hibit) = 2^(hibit) assuming hibit >= 1
+        
+        return &this->memory[hibit - highest_bit(FIRST_BUCKET_SIZE)][new_idx];
+    }
 public:
-    Vector();
+    Vector(){
+        T** buckets = new T*[1];
+        buckets[0] = new T[FIRST_BUCKET_SIZE];
+
+        this->memory = buckets; 
+    }
 
     // vector functions
     void resize(size_t size);
@@ -21,8 +56,13 @@ public:
     T pop_back();
 
     // random accesses
-    void write_at(size_t idx, T val);
-    T read_at(size_t idx);
+    void write_at(size_t idx, T val){
+        *at(idx) = val;
+    }
+
+    T read_at(size_t idx){
+        return *at(idx);
+    }
 
     // other
     size_t size();
--- main.cpp
+++ main.cpp
@@ -3,4 +3,8 @@
 
 int main(){
     std::cout << "hello world" << std::endl;
+    lockfree::Vector<int> x;
+    std::cout<<x.read_at(0)<<std::endl;
+    x.write_at(0,1);
+    std::cout<<x.read_at(0)<<std::endl;
 }
